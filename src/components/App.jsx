d154f8c10dbec63e3b61db71554c22e321023b3f
archis <vazearchis@gmail.com>
2022-07-08 19:57:43 +0530

    part1 done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,12 +18,15 @@ function App(props) {
 
 
     let [moviesArr, setMoviesArr] = useState([]);
-    let [loading, setLoading] = useState(true)
+    let [loading, setLoading] = useState(true);
+
+
 
     //get popular movies
 
 
-    let posterPath = "http://image.tmdb.org/t/p/w500"
+    let posterPath = "http://image.tmdb.org/t/p/w500";
+    // let searchUrl = "https://api.themoviedb.org/3/search/movie?api_key=14fafe6d7792756ff3a9c32c527eae57&query="
 
     useEffect(() => {
         setTimeout(() => setLoading(false), 2000)
@@ -32,15 +35,36 @@ function App(props) {
             .then(data => setMoviesArr(data.results))
     }, [])
 
+    function Search(movieName) {
+        console.log(movieName);
+        setLoading(true)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=14fafe6d7792756ff3a9c32c527eae57&query=${movieName}`)
+            .then(response => response.json())
+            .then(data => setMoviesArr(data.results))
+        setTimeout(() => {
+            setLoading(false)
+        }, 2000)
+
+
+    }
+
     return (
 
         <>
             {loading === false ? (
+                
                 <div className="main-container">
                     <div className="header">
                         <div className="header-title">Popular Movies Today</div>
                         <div className="search-container">
-                            <input placeholder="Search for a movie" className="search-input" type="text" />
+                            <input onKeyPress={(e) => {
+                                if (e.key === 'Enter') {
+                                    Search(e.target.value);
+                                    e.target.blur()
+                                    e.preventDefault();
+                                }
+                            }}
+                                placeholder="Search for a movie" className="search-input" type="text" />
                         </div>
                     </div>
                     <div className="container">
@@ -50,20 +74,23 @@ function App(props) {
                             movieOverview = arr.overview;
                             movieRating = arr.vote_average;
                             movieID = arr.id;
-                            return (
-                                <Link key={movieID} to={`/${movieID}`} >
-                                    <MovieCard key={movieID} movieTitle={movieTitle} moviePoster={moviePoster} movieOverview={movieOverview} movieRating={movieRating}
-                                        movieID={movieID} />
-                                </Link>
-                            )
+                            if (arr.poster_path) {
+                                return (
+                                    <Link key={movieID} to={`/${movieID}`} >
+                                        <MovieCard key={movieID} movieTitle={movieTitle} moviePoster={moviePoster} movieOverview={movieOverview} movieRating={movieRating}
+                                            movieID={movieID} />
+                                    </Link>
+                                )
+                            } else return (<span key={movieID}></span>)
                         })}
                     </div>
-
                 </div>
+
             ) : (
                 <LoadingScreen />
             )
             }
+
         </>
 
 
--- src/components/MoviePage.jsx
+++ src/components/MoviePage.jsx
@@ -1,12 +1,63 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import LoadingScreen from "./LoadingScreen";
 
 function MoviePage(props) {
 
     let { movieID } = useParams();
-    
     console.log(movieID)
+
+    let [movieObj, setMoviesObj] = useState([]);
+    let [loading, setLoading] = useState(true);
+
+
+
+
+
+
+    let posterPath = "http://image.tmdb.org/t/p/w500"
+
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 2000)
+        fetch(`https://api.themoviedb.org/3/movie/${movieID}?api_key=14fafe6d7792756ff3a9c32c527eae57`)
+            .then(response => response.json())
+            .then(data => setMoviesObj(data))
+    })
+
+
+
+
     return (
-        <h1>{movieID}</h1>
+        <>
+
+            {loading === false ? (
+
+                <div className="mp-main-container">
+                    <img className="backdrop" src={posterPath + movieObj.backdrop_path} alt="" />
+                    <div className="backdrop-overlay"></div>
+
+                    <div className="movie-page-container">
+                        <div className="mp-poster">
+                            <img src={posterPath + movieObj.poster_path} alt="" />
+                        </div>
+                        <div className="mp-info">
+                            <div className="mp-title">{movieObj.original_title}</div>
+                            <div className="mp-tagline">{movieObj.tagline}</div>
+                            <div className="mp-overview">{movieObj.overview}</div>
+
+                        </div>
+                    </div>
+                </div>
+
+            ) : (
+                <LoadingScreen />
+            )
+            }
+
+        </>
+
+
     )
 }
 
