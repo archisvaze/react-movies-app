8f296f827571669561b241b8480e65c9c74fab2c
archis <vazearchis@gmail.com>
2022-07-12 11:13:25 +0530

    done

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -2,6 +2,7 @@ function Cast(props) {
     let castObj = props.castObj;
     let name = castObj.name;
     let profilePath = "http://image.tmdb.org/t/p/w500" + castObj.profile_path;
+    
     return (
         <div className="cast">
             <img src={profilePath} alt="" className="profile-pic" />
