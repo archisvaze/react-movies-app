2b77926d2dd0423344a246ddd8bb9b6abfc87135
archis <vazearchis@gmail.com>
2022-07-12 11:29:11 +0530

    done

--- src/components/Cast.jsx
+++ src/components/Cast.jsx
@@ -2,13 +2,14 @@ function Cast(props) {
     let castObj = props.castObj;
     let name = castObj.name;
     let profilePath = "http://image.tmdb.org/t/p/w500" + castObj.profile_path;
-    
-    return (
-        <div className="cast">
-            <img src={profilePath} alt="" className="profile-pic" />
-            <div className="name">{name}</div>
-        </div>
-    )
+    if (castObj.profile_path !== null) {
+        return (
+            <div className="cast">
+                <img src={profilePath} alt="" className="profile-pic" />
+                <div className="name">{name}</div>
+            </div>
+        )
+    } else return (<></>)
 }
 
 export default Cast;
